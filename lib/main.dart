dc2c80c72abcbb92be342961400d4d5ea9a4b4fd
drunisa007 <newarunkatwal@gmail.com>
2022-01-24 19:55:44 +0630

    adding route page and creating sample home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'app/helper/apptheme_data.dart';
+import 'app/helper/app_theme_data.dart';
+import 'app/route/app_pages.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Ezsy Flutter App',
+      title: 'Kind Cash Pro',
       theme: appThemeData, // setting theme data
       debugShowCheckedModeBanner: false,
-      //getPages: AppPages.pages, // getting routes
+      getPages: AppPages.pages, // getting routes
       //initialRoute: Routes.INITIAL, // setting initial route for app
     );
   }
