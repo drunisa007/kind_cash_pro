ff6267c75b9f497f4efa2844e9b7c15d46c34070
drunisa007 <newarunkatwal@gmail.com>
2022-01-24 20:19:25 +0630

    final project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:device_preview/device_preview.dart';
 import 'app/helper/app_theme_data.dart';
 import 'app/route/app_pages.dart';
 
 void main() {
   runApp(const MyApp());
+  // runApp(DevicePreview(
+  //   enabled: !kReleaseMode,
+  //   builder: (context) => MyApp(), // Wrap your app
+  // ));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Kind Cash Pro',
+      builder: DevicePreview.appBuilder,
       theme: appThemeData, // setting theme data
       debugShowCheckedModeBanner: false,
       getPages: AppPages.pages, // getting routes
