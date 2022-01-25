01371616bb6d2317f8ec9bd8ae223ffeadb7f9ed
drunisa007 <newarunkatwal@gmail.com>
2022-01-25 23:01:21 +0630

    adding partner folder'

---
+++ lib/app/ui/partner/partner_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PartnerPage extends StatelessWidget {
+  const PartnerPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
