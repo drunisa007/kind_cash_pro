b25c041ad3b7348ce92b7ee33256ab56defee636
drunisa007 <newarunkatwal@gmail.com>
2022-01-25 23:02:07 +0630

    creating donor page

---
+++ lib/app/ui/donor/donar_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DonorPage extends StatelessWidget {
+  const DonorPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
